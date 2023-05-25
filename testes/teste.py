5e72d640ec1320df356ffac5a54b2e28f3da50e2
Lyinno <109544399+Lyinno@users.noreply.github.com>
2023-05-25 10:29:52 -0300

    Add files via upload

--- testes/teste.py
+++ testes/teste.py
@@ -51,3 +51,45 @@ def teste_atualizacao_preco_e_nome():
     print("Passou no caso 2")
   else:
     print("Não passou no caso 2")
+
+
+#mocking
+def remove_jogo():
+    return
+
+def teste_remove_jogo():
+    print("remove_jogo")
+    #estrutura ficticia
+    estrutura = {"nome1":0, "nome2":10, "nome3":21}
+    nome_invalido = "nome0"
+    nome_valido = "nome1"
+    estruturaAux = estrutura
+
+    #testando remoção de jogo invalido
+    remove_jogo(nome_invalido, estrutura)
+    if estrutura == estruturaAux:
+        print("Passou no caso 1 (nome_nao_presente)")
+    else:
+        print("Não passou no caso 1 (nome_nao_presente)")
+        return 1
+
+    #testando remoção de jogo valido
+    remove_jogo(nome_valido,estrutura)
+    if nome_valido not in estrutura.keys():
+        print("Passou no caso 3 (nome_presente)")
+    else:
+        print("Não passou no caso 3 (nome_presente)")
+        return 1
+
+    #testando remoção de jogo já removido
+    estruturaAux = estrutura
+    remove_jogo(nome_valido,estrutura)
+    if estruturaAux == estrutura:
+        print("Passou no caso 2 (nome_ja_removido)")
+    else:
+        print("Não passou no caso 2 (nome_ja_removido)")
+        return 1
+
+    return 0
+
+
