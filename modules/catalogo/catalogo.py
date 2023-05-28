83ca41b6420ed06b9152151809664f0ed9c9c8b5
joaogpd <joaogdrumond@hotmail.com>
2023-05-28 16:53:01 -0300

    added alterar nome e preco functions

---
+++ modules/catalogo/catalogo.py
@@ -0,0 +1,14 @@
+def alterar_nome(estrutura, nome, nome_novo):
+  if nome not in estrutura.keys():
+    return -1
+  else:
+    estrutura[nome_novo] = estrutura[nome]
+    del estrutura[nome_novo]
+    return 0
+  
+def alterar_preco(estrutura, nome, preco_novo):
+  if nome not in estrutura.keys():
+    return -1
+  else:
+    estrutura[nome] = preco_novo
+    return 0
