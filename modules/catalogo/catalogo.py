c9b0bb220aee4ebd51ea0104a816ab05c0491886
joaogpd <joaogdrumond@hotmail.com>
2023-06-03 14:33:14 -0300

    added exibe_todos_catalogo function and added it to all

--- modules/catalogo/catalogo.py
+++ modules/catalogo/catalogo.py
@@ -1,4 +1,4 @@
-__all__ = ["alterar_nome", "alterar_preco"]
+__all__ = ["alterar_nome", "alterar_preco", "exibe_todos_catalogo"]
 
 def alterar_nome(estrutura, nome, nome_novo):
   if nome not in estrutura.keys():
@@ -14,3 +14,7 @@ def alterar_preco(estrutura, nome, preco_novo):
   else:
     estrutura[nome] = preco_novo
     return 0
+
+def exibe_todos_catalogo(estrutura):
+  for i in estrutura.keys():
+    print("Nome: {} Preço: {}".format(i, estrutura[i]))
