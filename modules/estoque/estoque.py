746f1e4d996980eddb06bb2c2cf90cad17aa7256
joaogpd <joaogdrumond@hotmail.com>
2023-06-03 23:02:20 -0300

    adicionada especificacao de funcao para exibe_todo_estoque em estoque.py

--- modules/estoque/estoque.py
+++ modules/estoque/estoque.py
@@ -8,7 +8,28 @@ def remove_jogo(nome, estrutura):
         return 1 #Jogo removido com sucesso
     return 0 #Jogo nao encontrado (já removido)
 
+"""
+** Objetivo: exibir todos os itens da estrutura estoque
+** Descrição detalhada:
+- Valida o tipo do parâmetro recebido
+- Confere se a estrutura está vazia
+- Imprime todos as chaves presentes na estrutura
+** Acoplamento
+* Parâmetro:
+- estrutura -> dict: estrutura que deve ter chaves impressas
+* Retornos:
+- msgerro1: caso de 'estrutura' não ser dict
+- msgerro2: caso de 'estrutura' estar vazia
+- msgsucesso: caso de sucesso
+** Condições de acoplamento:
+* Assertivas de entrada:
+- A função recebe um parâmetro
+* Assertivas de saída:
+- Todos as chaves da estrutura foram impressas
+"""
 def exibe_todos_estoque(estrutura):
+  if not isinstance(estrutura, dict):
+    return -2
   if not estrutura:
     return -1 # Estrutura vazia. Dicionario vazio "valued" como "false"
   for i in estrutura.keys():
